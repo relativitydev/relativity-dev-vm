942803e1df5a72acf54fd6bf951350cb5596f419
Chandra Alimeti <chandra.alimeti@relativity.com>
2018-02-06 15:12:04 -0600

    minor update to the default recipe

--- Chef/Cookbooks/Relativity/recipes/default.rb
+++ Chef/Cookbooks/Relativity/recipes/default.rb
@@ -2,20 +2,9 @@ log 'Starting Default recipe'
 start_time = DateTime.now
 log "cookbook_start_time(#{cookbook_name}): #{start_time}"
 
-# Setup Windows
-include_recipe 'Relativity::default_windows'
-
-# Setup Pre-Relativity
-include_recipe 'Relativity::default_pre_relativity'
-
-# Setup Relativity
-include_recipe 'Relativity::default_relativity'
-
-# Setup Post Relativity
-include_recipe 'Relativity::default_post_relativity'
-
-# Configure Invariant
-include_recipe 'Relativity::post_relativity_configure_invariant'
+###################################################
+# Please refer to Vagrantfile or kitchen.yml files
+###################################################
 
 end_time = DateTime.now
 log "cookbook_end_Time(#{cookbook_name}): #{end_time}"
