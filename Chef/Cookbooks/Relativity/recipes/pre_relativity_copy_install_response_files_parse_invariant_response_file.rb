a21da2ca95e0aaa0bc8f405ee8fe743734262bba
Marlon Smith <msmith@kcura.com>
2018-02-07 14:02:55 -0600

    Updated Parsing Logic

--- Chef/Cookbooks/Relativity/recipes/pre_relativity_copy_install_response_files_parse_invariant_response_file.rb
+++ Chef/Cookbooks/Relativity/recipes/pre_relativity_copy_install_response_files_parse_invariant_response_file.rb
@@ -12,26 +12,32 @@ copy_file_to_vm_from_host "copy_relativity_response_file" do
 end
 
 # Read File into String
-::File.open(response_file_path, "r:bom|utf-8") do |f|
-	f.each_line do |line|
-
-		# Remove surrounding whitespace
-		clean_line = line.strip
-
-		# Skip empty lines and skip lines lines that start with pound sign
-		if clean_line != "" && clean_line.length > 0 && clean_line[0] != "#"
-
-			# Split string into array to target Response File Properties
-			# INSTALLPRIMARYDATABASE=0 -> we only want "INSTALLPRIMARYDATABASE"
-			line_elements = clean_line.split("=")
-
-			if line_elements.length > 0
-				# Add response file property to node array
-				node.default['invariant']['response_file']['parsed_values'] << {name: line_elements[0].strip, value: ""}
-			end
-		end
+ruby_block "Read Invariant Response File" do
+	block do
+		  File::open(response_file_path, "r:bom|utf-8") do |f|
+			  values = Array.new 
+			  f.each_line do |line|
+		  
+				  # Remove surrounding whitespace
+				  clean_line = line.strip
+		  
+				  # Skip empty lines and skip lines lines that start with pound sign
+				  if clean_line != "" && clean_line.length > 0 && clean_line[0] != "#"
+		  
+					  # Split string into array to target Response File Properties
+					  # INSTALLPRIMARYDATABASE=0 -> we only want "INSTALLPRIMARYDATABASE"
+					  line_elements = clean_line.split("=")
+		  
+					  if line_elements.length > 0
+						  # Add response file property to node array
+						  values.push( Hash["name" => line_elements[0].strip, "value" => ""] )
+					  end
+				  end
+			  end
+			  node.set['invariant']['response_file']['parsed_values'] = values
+		  end
+	  end
 	end
-end
 
 end_time = DateTime.now
 log "recipe_end_Time(#{recipe_name}): #{end_time}"
