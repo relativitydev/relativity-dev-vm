1d5cd0a5ffa68c3f3fe0f1754499341759d402bd
Marlon Smith <msmith@kcura.com>
2018-02-09 10:59:19 -0600

    Updated retry logic

--- Chef/Cookbooks/Relativity/libraries/Relativity_RetryHelper.rb
+++ Chef/Cookbooks/Relativity/libraries/Relativity_RetryHelper.rb
@@ -1,76 +1,24 @@
-# module Relativity
-#     module Helper
-  
-#       include Chef::Mixin::ShellOut
-  
-#       def execute_code_with_retry(code, max_retry, cmd_returns_value)
-#         retry_cnt = 0
-#         maximum_retry = max_retry
-#         result = nil
-#         error  = nil
-
-#         while retry_cnt < maximum_retry && result.nil?
-#             begin
-#                 result = eval(code) 
-#             rescue Exception => e
-#                 error = e.to_s
-#             end
-#             retry_cnt += 1
-#           end
-
-#         if result and cmd_returns_value == true
-#             result
-#         elseif error
-#             raise error
-#         end
-        
-#       end
-#     end
-#   end
-
-
 class Chef::Recipe::RetryHelper
   
       include Chef::Mixin::ShellOut
-  
-      def self.execute_code_with_retry(code, max_retry, cmd_returns_value)
-        retry_cnt = 0
-        maximum_retry = max_retry
-        result = nil
-        error  = nil
-
-        while retry_cnt == 0 or (retry_cnt < maximum_retry && result.nil? && !error.nil?)
-            begin
-                puts "begin start"
-                result = eval(code) 
-                puts "results is nil = #{result.nil?}"
-            rescue Exception => e
-                puts "rescue start"
-                error = e.to_s
-                puts error
-            end
-            retry_cnt += 1
-          end
-
-        if result and cmd_returns_value == true
-            puts result
-        elsif error
-            raise error
-        end
-        
-      end
 
       def self.execute_rest_call(http_obj, request, max_retry, err_msg)
         retry_cnt = 0
         maximum_retry = max_retry
         result = nil
-        error  = nil
+        error = nil
+        success = false
 
-        while retry_cnt == 0 or (retry_cnt < maximum_retry && result.nil? && !error.nil?)
+        while retry_cnt < maximum_retry && success == false
             begin
+                error = nil        
                 result = http_obj.request(request)
-                if !result.is_a? Net::HTTPSuccess
-                    raise err_msg +" " +result.message + " " +result.body
+                if result.is_a?(Net::HTTPOK) || result.is_a?(Net::HTTPSuccess)
+                    success = true
+                elsif result.is_a?(Net::HTTPUnauthorized)
+                    error = err_msg +" " +"Unauthorized Request, double check credentials"
+                else
+                    error = err_msg +" " +result.message + " " +result.body
                 end
             rescue Exception => e
                 error = e.to_s
@@ -78,7 +26,7 @@ class Chef::Recipe::RetryHelper
             retry_cnt += 1
           end
 
-        if result
+        if result && error.nil?
             result
         elsif error
             raise error
--- Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_add_processing_to_default_resource_pool.rb
+++ Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_add_processing_to_default_resource_pool.rb
@@ -115,10 +115,10 @@ response_json = JSON.parse(response.body)
 node.run_state['processing_location_artifact_id'] = response_json['Results'][0]['Artifact ID']
 log "processing_location_artifact_id = #{node.run_state['processing_location_artifact_id']}"
 
-
-# Add Processing Server Location to Default Resource Pool
-log "Adding Processing Server Location to 'Default' Resource Pool."
-url = "http://#{node['windows']['hostname']}/Relativity.Rest/api/Relativity.Services.ResourcePool.IResourcePoolModule/Resource%20Pool%20Service/AddProcessingSourceLocationAsync"
+# Check if Processing Server Location already added to Default Resource Pool
+processing_server_already_exists_in_resourcepool = false
+log "Checking to see if Processing Server Location already exists in 'Default' Resource Pool."
+url = "http://#{node['windows']['hostname']}/Relativity.Rest/api/Relativity.Services.ResourcePool.IResourcePoolModule/Resource%20Pool%20Service/GetProcessingSourceLocationsAsync"
 uri = URI(url)
 http = Net::HTTP.new(uri.host, uri.port)
 request = Net::HTTP::Post.new(uri.request_uri)
@@ -130,19 +130,48 @@ request_body_json = JSON.parse(
   {
     "resourcePool": {
       "ArtifactID": #{node.run_state['default_resource_pool_artifact_id']}
-    },
-    "ProcessingSourceLocation": {
-      "ArtifactID": #{node.run_state['processing_location_artifact_id']}
     }
   }
   QUERY
 )
 request.body = request_body_json.to_json
-error_message = "An unexpected error occured when adding Processing Server Location to 'Default' Resource Pool. "
+error_message = "An unexpected error occured when checking to see if Processing Server Location already exists in 'Default' Resource Pool."
 response = RetryHelper.execute_rest_call(http, request, 3, error_message)
-is_http_request_success = (response.is_a? Net::HTTPSuccess)
-if is_http_request_success
-  log "Added Processing Server Location to 'Default' Resource Pool."
+response_json = JSON.parse(response.body)
+if response_json.length > 0 && response_json[0]['ArtifactID'] == node.run_state['processing_location_artifact_id'].to_i
+  processing_server_already_exists_in_resourcepool = true
+  log "Processing Server Location already added to Default Resource Pool"
+end
+
+if processing_server_already_exists_in_resourcepool == false
+	# Add Processing Server Location to Default Resource Pool
+	log "Adding Processing Server Location to 'Default' Resource Pool."
+	url = "http://#{node['windows']['hostname']}/Relativity.Rest/api/Relativity.Services.ResourcePool.IResourcePoolModule/Resource%20Pool%20Service/AddProcessingSourceLocationAsync"
+	uri = URI(url)
+	http = Net::HTTP.new(uri.host, uri.port)
+	request = Net::HTTP::Post.new(uri.request_uri)
+	request.basic_auth(node['relativity']['admin']['login'], node['relativity']['admin']['password'])
+	request['X-CSRF-Header'] = ' '
+	request['Content-Type'] = 'application/json'
+	request_body_json = JSON.parse(
+	<<-QUERY
+	{
+		"resourcePool": {
+		"ArtifactID": #{node.run_state['default_resource_pool_artifact_id']}
+		},
+		"ProcessingSourceLocation": {
+		"ArtifactID": #{node.run_state['processing_location_artifact_id']}
+		}
+	}
+	QUERY
+	)
+	request.body = request_body_json.to_json
+	error_message = "An unexpected error occured when adding Processing Server Location to 'Default' Resource Pool. "
+	response = RetryHelper.execute_rest_call(http, request, 3, error_message)
+	is_http_request_success = (response.is_a? Net::HTTPSuccess)
+	if is_http_request_success
+		log "Added Processing Server Location to 'Default' Resource Pool."
+	end
 end
 
 end_time = DateTime.now
