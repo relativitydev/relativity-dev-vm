9a24ff8470e8f2b1145bce73bfee92e78dbe3e23
Chandra Alimeti <chandra.alimeti@relativity.com>
2018-02-22 10:15:29 -0600

    changed the order of copy and parse of response file to avoid some issues we faced.

--- Chef/Cookbooks/Relativity/recipes/pre_relativity_copy_install_response_files_parse_invariant_response_file.rb
+++ Chef/Cookbooks/Relativity/recipes/pre_relativity_copy_install_response_files_parse_invariant_response_file.rb
@@ -4,40 +4,29 @@ custom_log 'custom_log' do msg "recipe_start_time(#{recipe_name}): #{start_time}
 
 response_file_path = "#{node['invariant']['response_file']['destination_folder']}\\#{node['invariant']['response_file']['file_name_original']}"
 
-# Copy response file
-copy_file_to_vm_from_host 'copy_relativity_response_file' do
-  file_source "#{node['invariant']['response_file']['source_folder']}\\#{node['invariant']['response_file']['file_name']}"
-  file_destination response_file_path
-  file_destination_folder node['invariant']['response_file']['destination_folder']
-end
-
 # Read File into String
-ruby_block 'Read Invariant Response File' do
-  block do
-    File::open(response_file_path, "r:bom|utf-8") do |f|
-      values = Array.new 
-      f.each_line do |line|
-        # Remove surrounding whitespace
-        clean_line = line.strip
+File::open(response_file_path, "r:bom|utf-8") do |f|
+  values = Array.new 
+  f.each_line do |line|
+    # Remove surrounding whitespace
+    clean_line = line.strip
 
-        # Skip empty lines and skip lines lines that start with pound sign
-        if clean_line != "" && clean_line.length > 0 && clean_line[0] != "#"
-          # Split string into array to target Response File Properties
-          # INSTALLPRIMARYDATABASE=0 -> we only want "INSTALLPRIMARYDATABASE"
-          line_elements = clean_line.split("=")
+    # Skip empty lines and skip lines lines that start with pound sign
+    if clean_line != "" && clean_line.length > 0 && clean_line[0] != "#"
+      # Split string into array to target Response File Properties
+      # INSTALLPRIMARYDATABASE=0 -> we only want "INSTALLPRIMARYDATABASE"
+      line_elements = clean_line.split("=")
 
-          if line_elements.length > 0
-            # Add response file property to node array
-            name = line_elements[0].strip
-            value = ''
-            values.push( Hash['name' => name, 'value' => value])
-            puts "Invariant Response File - Parsed Value: #{name}"
-          end
-        end
+      if line_elements.length > 0
+        # Add response file property to node array
+        name = line_elements[0].strip
+        value = ''
+        values.push( Hash['name' => name, 'value' => value])
+        puts "Invariant Response File - Parsed Value: #{name}"
       end
-      node.set['invariant']['response_file']['parsed_values'] = values
     end
   end
+  node.default['invariant']['response_file']['parsed_values'] = values
 end
 
 end_time = DateTime.now
