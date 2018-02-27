258f19b8d214bcd59750226087fe052f2247e009
Marlon Smith <msmith@kcura.com>
2018-02-27 08:23:11 -0600

    Added validation around Invariant Configuration

--- Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_add_processing_to_default_resource_pool.rb
+++ Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_add_processing_to_default_resource_pool.rb
@@ -54,6 +54,10 @@ response_json.each do |type|
   end
 end
 
+if node.run_state['resource_server_types_choice_processing_artifact_id'] == nil
+  raise "Unable to find Worker Manager Server"
+end
+
 # Add Processing Server to Default Resource Pool
 custom_log 'custom_log' do msg "Adding Processing Server to 'Default' Resource Pool." end
 url = "http://#{node['windows']['hostname']}/Relativity.Rest/api/Relativity.Services.ResourcePool.IResourcePoolModule/Resource%20Pool%20Service/AddServerAsync"
@@ -86,59 +90,62 @@ if is_http_request_success
   custom_log 'custom_log' do msg "Added Processing Server to 'Default' Resource Pool." end
 end
 
+# Processing Source Location should have been created via powershell, query the choice list for 5 minutes and wait until it is available
+processing_location_exists = false
+ple_timer = 0
+ple_interval = 10
+ple_max_time = 300
 
-# Query Processing location
-custom_log 'custom_log' do msg 'Querying for Processing location.' end
-url = "http://#{node['windows']['hostname']}/Relativity.REST/Relativity/Choice/QueryResult"
-uri = URI(url)
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri)
-request.basic_auth(node['relativity']['admin']['login'], node['relativity']['admin']['password'])
-request['X-CSRF-Header'] = ' '
-request['Content-Type'] = 'application/json'
-request_body_json = JSON.parse(
-  <<-QUERY
-  {
-    "condition": " 'Choice Type ID' == 1000017",
-    "fields": [
-      "*"
-    ]
-  }
-  QUERY
-)
-request.body = request_body_json.to_json
-error_message = 'An unexpected error occured when querying Processing location.'
-response = RetryHelper.execute_rest_call(http, request, 3, error_message)
-response_json = JSON.parse(response.body)
-if response_json['TotalResultCount'] > 0
-  node.run_state['processing_location_artifact_id'] = response_json['Results'][0]['Artifact ID']
-  custom_log 'custom_log' do msg "processing_location_artifact_id = #{node.run_state['processing_location_artifact_id']}" end
-else
+while processing_location_exists == false && ple_timer < ple_max_time
+  # Sleep
+  sleep(ple_interval)
+    
+  # Query Processing location
+  custom_log 'custom_log' do msg 'Querying for Processing location.' end
+  url = "http://#{node['windows']['hostname']}/Relativity.REST/Relativity/Choice/QueryResult"
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.basic_auth(node['relativity']['admin']['login'], node['relativity']['admin']['password'])
+  request['X-CSRF-Header'] = ' '
+  request['Content-Type'] = 'application/json'
+  request_body_json = JSON.parse(
+    <<-QUERY
+    {
+      "condition": " 'Choice Type ID' == 1000017",
+      "fields": [
+        "*"
+      ]
+    }
+    QUERY
+  )
+  request.body = request_body_json.to_json
+  error_message = 'An unexpected error occured when querying Processing location.'
+  response = RetryHelper.execute_rest_call(http, request, 3, error_message)
+  response_json = JSON.parse(response.body)
+  if response_json['TotalResultCount'] > 0
+    node.run_state['processing_location_artifact_id'] = response_json['Results'][0]['Artifact ID']
+    processing_location_exists = true
+    custom_log 'custom_log' do msg "processing_location_artifact_id = #{node.run_state['processing_location_artifact_id']}" end
+  end
+
+  # Increment Interval
+  ple_timer += ple_interval
+end
+
+if ple_timer >= ple_max_time
+  custom_log 'custom_log' do msg "Timed out while querying processing source location" end
+  raise "Timed out while querying processing source location"
+end
+if processing_location_exists == false
+  custom_log 'custom_log' do msg "Unable to find Processing Location" end
   raise "Unable to find Processing Location"
 end
 
 # Check if Processing Server Location already added to Default Resource Pool
 processing_server_already_exists_in_resourcepool = false
 custom_log 'custom_log' do msg "Checking to see if Processing Server Location already exists in 'Default' Resource Pool." end
-url = "http://#{node['windows']['hostname']}/Relativity.Rest/api/Relativity.Services.ResourcePool.IResourcePoolModule/Resource%20Pool%20Service/GetProcessingSourceLocationsAsync"
-uri = URI(url)
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri)
-request.basic_auth(node['relativity']['admin']['login'], node['relativity']['admin']['password'])
-request['X-CSRF-Header'] = ' '
-request['Content-Type'] = 'application/json'
-request_body_json = JSON.parse(
-  <<-QUERY
-  {
-    "resourcePool": {
-      "ArtifactID": #{node.run_state['default_resource_pool_artifact_id']}
-    }
-  }
-  QUERY
-)
-request.body = request_body_json.to_json
-error_message = "An unexpected error occured when checking to see if Processing Server Location already exists in 'Default' Resource Pool."
-response = RetryHelper.execute_rest_call(http, request, 3, error_message)
+response = ProcessingHelper.query_resourcepool_processing_source_locations(node['windows']['hostname'], node['relativity']['admin']['login'], node['relativity']['admin']['password'], node.run_state['default_resource_pool_artifact_id'])
 response_json = JSON.parse(response.body)
 if response_json.length > 0 && response_json[0]['ArtifactID'] == node.run_state['processing_location_artifact_id'].to_i
   processing_server_already_exists_in_resourcepool = true
@@ -174,7 +181,39 @@ if processing_server_already_exists_in_resourcepool == false
   if is_http_request_success
     custom_log 'custom_log' do msg "Added Processing Server Location to 'Default' Resource Pool." end
   end
+
+  # Processing Sever Location should have been created above, query for 5 minutes and wait until it is available
+  processing_server_location_queryable = false
+  pslq_timer = 0
+  pslq_interval = 10
+  pslq_max_time = 300
+
+  while processing_server_location_queryable == false && pslq_timer < pslq_max_time
+    # Sleep
+    sleep(pslq_interval)
+
+    custom_log 'custom_log' do msg "Checking to see if Processing Server Location already exists in 'Default' Resource Pool." end
+    response = ProcessingHelper.query_resourcepool_processing_source_locations(node['windows']['hostname'], node['relativity']['admin']['login'], node['relativity']['admin']['password'], node.run_state['default_resource_pool_artifact_id'])
+    response_json = JSON.parse(response.body)
+    if response_json.length > 0 && response_json[0]['ArtifactID'] == node.run_state['processing_location_artifact_id'].to_i
+      processing_server_location_queryable = true
+      custom_log 'custom_log' do msg "Succesfully queried Processing Server Location in default resource pool" end
+    end
+
+    # Increment Interval
+    pslq_timer += pslq_interval
+  end
+  if pslq_timer >= pslq_max_time
+    custom_log 'custom_log' do msg "Timed out while querying processing server location in resource pool" end
+    raise "Timed out while querying processing server location in resource pool"
+  end
+  if processing_server_location_queryable == false
+    custom_log 'custom_log' do msg "Unable to find Processing Server Location in resource pool" end
+    raise "Unable to find Processing Server Location in resource pool"
+  end
 end
+
+
 
 end_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_end_Time(#{recipe_name}): #{end_time}" end
--- Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_update_worker_server_for_processing.rb
+++ Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_update_worker_server_for_processing.rb
@@ -2,45 +2,6 @@ custom_log 'custom_log' do msg 'Starting update worker server for processing' en
 start_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_start_time(#{recipe_name}): #{start_time}" end
 
-# uses kepler to query resource server table and get the worker server ArtifactID
-custom_log 'custom_log' do msg 'using kepler to query resource server table and get the worker server ArtifactID.' end
-node.run_state['agent'] = {}
-node.run_state['agent']['servers'] = {}
-node.run_state['agent']['servers']['agent'] = {}
-node.run_state['agent']['servers']['worker'] = {}
-url = "http://#{node['windows']['hostname']}/Relativity.Rest/api/Relativity.Services.ResourceServer.IResourceServerModule/Resource%20Server%20Manager/QueryAsync"
-uri = URI(url)
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri)
-request.basic_auth(node['relativity']['admin']['login'], node['relativity']['admin']['password'])
-request['X-CSRF-Header'] = ' '
-request['Content-Type'] = 'application/json'
-request_body_json = JSON.parse(
-  <<-QUERY
-  {
-    "Query": {}
-  }
-  QUERY
-)
-request.body = request_body_json.to_json
-error_message = 'An unexpected error occured when using kepler to query resource server table and get the worker server ArtifactID'
-response = RetryHelper.execute_rest_call(http, request, 3, error_message)
-response_json = JSON.parse(response.body)
-node.run_state['agent']['agent_server'] = response_json
-node.run_state['agent']['agent_server']['Results'].each do |key|
-  custom_log 'custom_log' do msg "Server name is #{key['Artifact']['ServerType']['Name']} and its Artifact ID is #{key['Artifact']['ServerType']['ArtifactID']}" end
-  if key['Artifact']['Name'].casecmp(node['windows']['hostname']) == 0 && key['Artifact']['ServerType']['Name'] == 'Worker'
-    custom_log 'custom_log' do msg key['Artifact']['ArtifactID'] end
-    node.run_state['agent']['servers']['worker']['artifact_id'] = key['Artifact']['ArtifactID']
-    custom_log 'custom_log' do msg "node.run_state['agent']['servers']['worker']['artifact_id'] = #{node.run_state['agent']['servers']['worker']['artifact_id']}" end
-  else
-    next
-  end
-end
-if node.run_state['agent']['servers']['worker']['artifact_id'].nil?
-  raise 'Worker artifact ID not found!'
-end
-
 # Kepler call to enable\license a worker
 custom_log 'custom_log' do msg 'Kepler call to enable\license a worker' end
 url = "http://#{node['windows']['hostname']}/Relativity.Rest/api/Relativity.Services.WorkerStatus.IWorkerStatusModule/WorkerStatus/EnableProcessingOnWorkerAsync"
@@ -53,7 +14,7 @@ request['Content-Type'] = 'application/json'
 request_body_json = JSON.parse(
   <<-QUERY
   {
-    "workerArtifactId": #{node.run_state['agent']['servers']['worker']['artifact_id']}
+    "workerArtifactId": #{node.run_state['worker_resource_server_artifact_id']}
   }
   QUERY
 )
@@ -74,7 +35,7 @@ request['Content-Type'] = 'application/json'
 request_body_json = JSON.parse(
   <<-QUERY
   {
-    "workerArtifactId": #{node.run_state['agent']['servers']['worker']['artifact_id']},
+    "workerArtifactId": #{node.run_state['worker_resource_server_artifact_id']},
     "categories": [
       "NativeImaging",
       "BasicImaging",
@@ -86,8 +47,13 @@ request_body_json = JSON.parse(
 )
 request.body = request_body_json.to_json
 error_message = 'An unexpected error occured when using Kepler call to add processing designated work to a worker'
-response = RetryHelper.execute_rest_call(http, request, 3, error_message)
-custom_log 'custom_log' do msg response.code end
+begin
+  response = RetryHelper.execute_rest_call(http, request, 3, error_message)
+  custom_log 'custom_log' do msg response.code end
+rescue Exception => e
+  # Exception eatin until we fullfil processing prerequisite apps.
+  custom_log 'custom_log' do msg "Error while enabling processing for Worker.S " +e.to_s end
+end
 
 end_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_end_Time(#{recipe_name}): #{end_time}" end
