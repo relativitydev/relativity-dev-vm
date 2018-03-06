76d533f8e7d6b1f2dd75d92a7d405a2269fc9c29
Chandra Alimeti <chandra.alimeti@relativity.com>
2018-03-06 16:44:38 -0600

    added code to update smtp/email instance settings.

--- Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_instance_setting_update_processingwebapipath.rb
+++ Chef/Cookbooks/Relativity/recipes/post_relativity_configure_invariant_instance_setting_update_processingwebapipath.rb
@@ -1,14 +1,14 @@
-custom_log 'custom_log' do msg 'Starting update ProcessingWebAPIPath' end
+custom_log 'custom_log' do msg 'Starting ProcessingWebAPIPath Instance Setting Update' end
 start_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_start_time(#{recipe_name}): #{start_time}" end
 
 # Get the full path to the SQLPS module.
 sqlps_module_path = ::File.join(ENV['programfiles(x86)'], 'Microsoft SQL Server\130\Tools\PowerShell\Modules\SQLPS')
 
-# Run update ProcessingWebAPIPath
-custom_log 'custom_log' do msg 'Setting CookieSecure Instance Setting to False.' end
-powershell_script 'relativity_http_setup' do
-    code <<-EOH
+# Update ProcessingWebAPIPath
+custom_log 'custom_log' do msg 'Updating RelativityWebAPI Instance Setting.' end
+powershell_script 'update_instance_setting - RelativityWebAPI' do
+  code <<-EOH
     Import-Module "#{sqlps_module_path}"
     Invoke-Sqlcmd -Query "
         UPDATE
@@ -21,9 +21,9 @@ powershell_script 'relativity_http_setup' do
     "
     EOH
 end
-custom_log 'custom_log' do msg 'Updated ProcessingWebAPIPath.' end
+custom_log 'custom_log' do msg 'Updated RelativityWebAPI Instance Setting.' end
 
 end_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_end_Time(#{recipe_name}): #{end_time}" end
 custom_log 'custom_log' do msg "recipe_duration(#{recipe_name}): #{end_time.to_time - start_time.to_time} seconds" end
-custom_log 'custom_log' do msg "Finished CookieSecure Instance Setting Update\n\n\n" end
+custom_log 'custom_log' do msg "Finished ProcessingWebAPIPath Instance Setting Update\n\n\n" end
