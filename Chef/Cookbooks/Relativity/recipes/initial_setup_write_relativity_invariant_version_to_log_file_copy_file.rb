f96e64c1aaab7d1285c5905944c9007d3c577f46
Chandra Alimeti <chandra.alimeti@relativity.com>
2018-05-10 09:47:30 -0500

    added logic to write Relativity and Invariant versions to log file.

--- Chef/Cookbooks/Relativity/recipes/initial_setup_write_relativity_invariant_version_to_log_file_copy_file.rb
+++ Chef/Cookbooks/Relativity/recipes/initial_setup_write_relativity_invariant_version_to_log_file_copy_file.rb
@@ -1,4 +1,4 @@
-custom_log 'custom_log' do msg 'Writing Relativity and Invariant Version Numbers to log file' end
+custom_log 'custom_log' do msg 'Copying Relativity and Invariant Version Numbers file to VM' end
 start_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_start_time(#{recipe_name}): #{start_time}" end
 
@@ -13,15 +13,7 @@ copy_file_to_vm_from_host 'copy_relativity_invariant_version_number_file' do
   file_destination_folder node['relativity']['install']['destination_folder']
 end
 
-if ::File.file?(version_number_file_destination)
-  custom_log 'custom_log' do msg 'Relativity and Invariant Version Number file exists on VM' end
-  file_content = ::File.read(version_number_file_destination)
-  custom_log 'custom_log' do msg "#{file_content}" end
-else
-  custom_log 'custom_log' do msg 'Relativity and Invariant Version Number file does NOT exists on VM' end
-end
-
 end_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_end_Time(#{recipe_name}): #{end_time}" end
 custom_log 'custom_log' do msg "recipe_duration(#{recipe_name}): #{end_time.to_time - start_time.to_time} seconds" end
-custom_log 'custom_log' do msg "Finished writing Relativity and Invariant Version Numbers to log file\n\n\n" end
+custom_log 'custom_log' do msg "Finished copying Relativity and Invariant Version Numbers file to VM\n\n\n" end
