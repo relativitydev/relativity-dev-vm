da9932deebb4523ea1f202ec756c51f30cde2240
Chandra Kaushik Alimeti <chandra.alimeti@relativity.com>
2018-11-14 11:43:59 -0600

    add code to clear datagridendpoint instance setting value which is pointing to a non-existent datagrid cluster.

--- Chef/Cookbooks/Relativity/recipes/pre_relativity_install_servicebus_sleep_5_minutes.rb
+++ Chef/Cookbooks/Relativity/recipes/pre_relativity_install_servicebus_sleep_5_minutes.rb
@@ -1,4 +1,4 @@
-custom_log 'custom_log' do msg 'Starting Service Bus install' end
+custom_log 'custom_log' do msg 'Sleeping for 5 minutes after Service Bus install' end
 start_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_start_time(#{recipe_name}): #{start_time}" end
 
@@ -8,4 +8,4 @@ sleep 300
 end_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_end_Time(#{recipe_name}): #{end_time}" end
 custom_log 'custom_log' do msg "recipe_duration(#{recipe_name}): #{end_time.to_time - start_time.to_time} seconds" end
-custom_log 'custom_log' do msg "Finished Service Bus install\n\n\n" end
+custom_log 'custom_log' do msg "Slept for 5 minutes after Service Bus install\n\n\n" end
