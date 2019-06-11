113780166fc6f76adca8d89454dd70ee513c8be4
Aaron Gilbert <aaron.gilbert@relativity.com>
2019-06-11 15:23:04 -0500

    IInstanceSettingsHelper added

---
+++ CSharp/DevVmPowershell/Helpers/InstanceSettingsHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Relativity.API;
+using Relativity.Services.ServiceProxy;
+
+namespace Helpers
+{
+	public class InstanceSettingsHelper : IInstanceSettingsHelper
+	{
+		private ServiceFactory ServiceFactory { get; }
+
+		public InstanceSettingsHelper(IConnectionHelper connectionHelper)
+		{
+			ServiceFactory = connectionHelper.GetServiceFactory();
+		}
+
+		public void UpdateInstanceSettings(string section, string name, string newValue)
+		{
+			
+		}
+	}
+}
