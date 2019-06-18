ddd71d46b6dcbea5efc8216f14ee371f4f33f2fd
Aaron Gilbert <aaron.gilbert@relativity.com>
2019-06-18 09:00:34 -0500

    Recycle Bin Helper works

--- CSharp/DevVmPowershell/Helpers/RecycleBinHelper.cs
+++ CSharp/DevVmPowershell/Helpers/RecycleBinHelper.cs
@@ -14,7 +14,7 @@ namespace Helpers
 		enum RecycleFlag : int
 		{
 			SHERB_NOCONFIRMATION = 0x00000001, // No confirmation, when emptying
-			SHERB_NOPROGRESSUI = 0x00000001, // No progress tracking window during the emptying of the recycle bin
+			SHERB_NOPROGRESSUI = 0x00000002, // No progress tracking window during the emptying of the recycle bin
 			SHERB_NOSOUND = 0x00000004 // No sound when the emptying of the recycle bin is complete
 		}
 
