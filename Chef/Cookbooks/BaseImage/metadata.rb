52aaf564dfde158a00a7696ca86cae82c4c20364
Chandra Kaushik Alimeti <chandra.alimeti@relativity.com>
2019-06-19 16:30:16 -0500

    updated metadata file with dependent cookbooks

--- Chef/Cookbooks/BaseImage/metadata.rb
+++ Chef/Cookbooks/BaseImage/metadata.rb
@@ -7,6 +7,10 @@ long_description 'Installs/Configures BaseImage'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
+depends 'powershell', '= 6.0.0'
+depends 'webpi', '~> 4.2.0'
+depends 'msoffice', '~> 0.1.0'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- Chef/Cookbooks/BaseImage/recipes/pre_relativity_install_windows_update_for_service_bus_defect.rb
+++ Chef/Cookbooks/BaseImage/recipes/pre_relativity_install_windows_update_for_service_bus_defect.rb
@@ -13,7 +13,7 @@ service_bus_defect_windows_udpate_installer_location = "#{node['service_bus']['d
 
 # Copy the service bus windows update file to install directory
 cookbook_file service_bus_defect_windows_udpate_installer_location do
-  source node['service_bus']['installer_file_name']
+  source node['service_bus']['defect_windows_update']['install']['file_name']
 end
 
 # Install windows update
