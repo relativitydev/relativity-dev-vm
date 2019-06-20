5849bdf376be1f03dc7177d32a7638df9bf19c59
Chandra Kaushik Alimeti <chandra.alimeti@relativity.com>
2019-06-20 10:17:31 -0500

    removed duplicate code.

--- Chef/Cookbooks/Relativity/recipes/default_relativity.rb
+++ Chef/Cookbooks/Relativity/recipes/default_relativity.rb
@@ -8,9 +8,6 @@ include_recipe 'Relativity::relativity_install'
 include_recipe 'Relativity::post_relativity_configure_invariant_instance_setting_update_processingwebapipath'
 include_recipe 'Relativity::post_relativity_instance_setting_update_cookie_secure'
 
-# Install Invariant
-include_recipe 'Relativity::relativity_install_invariant'
-
 end_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_end_Time(#{recipe_name}): #{end_time}" end
 custom_log 'custom_log' do msg "recipe_duration(#{recipe_name}): #{end_time.to_time - start_time.to_time} seconds" end
