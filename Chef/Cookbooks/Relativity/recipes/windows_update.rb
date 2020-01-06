b286aaee3c0a7c6f5ceb0d9073e6bd8d0eee37a7
Jake Martin <jake.martin@relativity.com>
2020-01-06 13:45:58 -0600

    added recipe to update windows

---
+++ Chef/Cookbooks/Relativity/recipes/windows_update.rb
@@ -0,0 +1,20 @@
+custom_log 'custom_log' do msg 'Starting Windows Updates' end
+    start_time = DateTime.now
+    custom_log 'custom_log' do msg "recipe_start_time(#{recipe_name}): #{start_time}" end
+    
+    powershell_script 'Install_Bootstrapper_And_Update_Windows' do
+      code <<-EOH
+        Invoke-WebRequest -UseBasicParsing -Uri https://boxstarter.org/bootstrapper.ps1 | Invoke-Expression; Get-Boxstarter -Force
+        Import-Module Boxstarter.WinConfig
+        Enable-MicrosoftUpdate
+        Restart-Service -Name "Windows Update"
+        Install-WindowsUpdate -AcceptEula -SuppressReboots
+        Write-Output "Updates complete."
+        EOH
+    end
+    
+    end_time = DateTime.now
+    custom_log 'custom_log' do msg "recipe_end_Time(#{recipe_name}): #{end_time}" end
+    custom_log 'custom_log' do msg "recipe_duration(#{recipe_name}): #{end_time.to_time - start_time.to_time} seconds" end
+    custom_log 'custom_log' do msg "Finished Windows Updates\n\n\n" end
+    
