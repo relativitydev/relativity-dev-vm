10d96a8f6b7eb933f4ab97197da912100205a363
Chandra Kaushik Alimeti <chandra.alimeti@relativity.com>
2020-06-16 16:46:02 -0500

    removed default password from the code.

--- CSharp/DevVmPowershell/Helpers.Tests.Integration/TestConstants.cs
+++ CSharp/DevVmPowershell/Helpers.Tests.Integration/TestConstants.cs
@@ -7,11 +7,11 @@
 		public const string SAMPLE_DATA_GRID_WORKSPACE_NAME = "Sample Data Grid Workspace";
 		public const string RELATIVITY_INSTANCE_WEB_SERVICE_URL = "http://" + RELATIVITY_INSTANCE_NAME + "/relativitywebapi/";
 		public const string RELATIVITY_ADMIN_USER_NAME = "relativity.admin@relativity.com";
-		public const string RELATIVITY_ADMIN_PASSWORD = "Test1234!";
+		public const string RELATIVITY_ADMIN_PASSWORD = "<password_removed_contact_relativity_plex_team>";
 		public const string SAMPLE_APPLICATION_FILE_PATH = @"TestFiles\RAP\Single-Object_Single-Field_Application_1.0.0.0.rap";
 		public const string AGENT_NAME = "Data Grid Audit Manager";
 		public const string SQL_USER_NAME = "eddsdbo";
-		public const string SQL_PASSWORD = "Test1234!";
+		public const string SQL_PASSWORD = "<password_removed_contact_relativity_plex_team>";
 		public const string TEST_ZIP_FILES_FOLDER_PATH = @"TestFiles\FilesToZip";
 		public const string ELASTIC_SEARCH_YML_FILE_PATH = @"TestFiles\DataGrid\elasticsearch.yml";
 		public const string ENVIRONMENT_VARIABLE_KCURA_JAVA_HOME = "KCURA_JAVA_HOME";
