33691aed8d1b859b981c8f787fa6143b2dc5e398
Saravanan Natarajan <saravanan.natarajan@relativity.com>
2021-07-20 15:03:20 -0500

    DEVOPS-127787: Updating disclaimer rap

--- Chef/Cookbooks/Relativity/recipes/post_relativity_powershell_module_add_disclaimer_acceptance_log.rb
+++ Chef/Cookbooks/Relativity/recipes/post_relativity_powershell_module_add_disclaimer_acceptance_log.rb
@@ -2,7 +2,7 @@ custom_log 'custom_log' do msg 'Starting Installing Disclaimer Acceptance Log Ra
 start_time = DateTime.now
 custom_log 'custom_log' do msg "recipe_start_time(#{recipe_name}): #{start_time}" end
 
-rap_file_path = win_friendly_path(File.join(Chef::Config[:file_cache_path], '\cookbooks\Relativity\files\default\Disclaimer_Acceptance_Log.rap'))
+rap_file_path = win_friendly_path(File.join(Chef::Config[:file_cache_path], '\cookbooks\Relativity\files\default\Disclaimer_Acceptance_Log (Ninebark 12-0 and Above).rap'))
 
 # Install Disclaimer Acceptance Log Rap
 custom_log 'custom_log' do msg 'Installing Disclaimer Acceptance Log Rap' end
